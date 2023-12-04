d816b7c2f1d387ac30ca5d437c73baf71e32d183
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-12-04 20:57:29 +0330

    webpack configured

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "./components/Menu";
+import data from "./data/recipes.json";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="hello">Hello Developers</h1>
+      <Menu recipes={data} />
+    </>
+  );
+};
+
+export default App;
